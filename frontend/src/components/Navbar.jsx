5c7030556c4711f81ce95e39046ac1471cc01391
UDAYhere <uday92689@gmail.com>
2025-11-09 14:33:50 +0530

    updating

--- frontend/src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from 'react-router-dom';
-export default function Navbar(){
-return (
-<nav className="nav">
-<Link to="/">Hide Message</Link>
-<Link to="/extract">Extract Message</Link>
-</nav>
-)
-}
